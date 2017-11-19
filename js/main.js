b4c6bd3a31f21649115e7de50f268eb4251a3a1f
Mina Parnian <minaparnian66@gmail.com>
2017-11-19 22:12:52 +1100

    on click event handler

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ new Vue({
   data: {
     name: 'Mina',
     age: 30,
-    image: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg"
+    image: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg",
+    value: '',
   },
   
   methods: {
     randomNum: function () {
       return Math.floor(Math.random() * (10 - 0)) + 0;
+    },
+    alert: function(event){
+      alert("button clicked")
     }
   }
   

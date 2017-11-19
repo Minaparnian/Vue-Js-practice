14c45f411146f8e05c48e1d310efa2aa8e314f7f
Mina Parnian <minaparnian66@gmail.com>
2017-11-19 21:26:20 +1100

    .bind

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ new Vue({
   el: '#exercise',
   data: {
     name: 'Mina',
-    age: 30
+    age: 30,
+    image: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg"
   },
   
   methods: {
     randomNum: function () {
-      
+      return Math.floor(Math.random() * (10 - 0)) + 0;
     }
   }
   

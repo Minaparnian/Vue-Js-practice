efafc2537ed88347df033ce5c974d7d9b0e9046e
Mina Parnian <minaparnian66@gmail.com>
2017-11-20 12:08:21 +1100

    style exercise

--- js/main.js
+++ js/main.js
@@ -1,40 +1,11 @@
 new Vue({
   el: '#exercise',
   data: {
-    name: 'Mina',
-    age: 30,
-    image: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg",
-    // value: '',
-    value: 0,
-    
+
   },
-  
-  computed: {
-    result: function() {
-      return this.value ==37 ? 'Done' : 'not there yet';
-    }
-  },
-    
-  
-  watch: {
-    result: function () {
-      var vm = this;
-      setTimeout(function(){
-        vm.value = 0;
-      }, 5000);
-  }
-},
-    
-  
-  
   methods: {
-    randomNum: function () {
-      return Math.floor(Math.random() * (10 - 0)) + 0;
-    },
-    alert: function(event){
-      alert("button clicked")
-    },
+    startEffect: function() {
     
+    }
   }
-  
 });
